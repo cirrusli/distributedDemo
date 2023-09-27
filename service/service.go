127a57ea3bacdc49f98445409830d3850f5231c1
CodeXzzz <codexzzz@outlook.com>
2023-09-27 22:00:25 +0800

    add the chatGPT's comment

--- service/service.go
+++ service/service.go
@@ -21,8 +21,14 @@ func Start(ctx context.Context, host, port string,
 }
 
 // 操作服务状态（启动或关闭）(包级函数）
+// 这段代码实现了启动一个 HTTP 服务器的功能。它使用了 context.Context 来处理关闭服务器的操作。代码的执行流程如下：
+// 首先，通过 context.WithCancel 创建一个新的 context.Context，并启动一个新的 goroutine。
+// 在新的 goroutine 中，调用 srv.ListenAndServe 方法来启动 HTTP 服务器。如果服务器无法启动，则调用 registry.ShutdownService 方法来关闭服务器，并取消上下文。
+// 然后，另外启动一个 goroutine，等待用户输入。如果用户按下任意键，则调用 registry.ShutdownService 方法关闭服务器，并调用 srv.Shutdown 方法取消上下文。
+// 需要注意的是，在这段代码中，srv.Addr 变量并未使用到 host 变量，也就是说，只有 port 参数被用来作为服务器的端口。
 func startService(ctx context.Context, serviceName registry.ServiceName,
 	host, port string) context.Context {
+
 	ctx, cancel := context.WithCancel(ctx)
 	var srv http.Server
 	//host+port
